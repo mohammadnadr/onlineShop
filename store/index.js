03f0a6878291a9d6089b8f27c275381e8fe1e379
mohamaddnadr <amoo.nadr81@gmail.com>
2023-08-11 20:31:49 +0330

    add snackbar to store

--- store/index.js
+++ store/index.js
@@ -1,9 +1,19 @@
 export const state = () => ({
   basket: [],
+  snackbar: {
+    content: '',
+    color: '',
+    timeout: ''
+  },
 
 });
 
 export const mutations = {
+  showMessage(state, payload) {
+    state.snackbar.content = payload.content;
+    state.snackbar.color = payload.color;
+    state.snackbar.timeout = payload.timeout;
+  },
   addToBasket(state, payload) {
     state.basket.push(payload);
     sessionStorage.setItem('basket', JSON.stringify(state.basket))
