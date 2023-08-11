e35f6642d549313899a935ca785cd12ce5f11cfe
mohamaddnadr <amoo.nadr81@gmail.com>
2023-08-11 20:17:58 +0330

    rename and new file

---
+++ components/ProductItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+  <v-card>
+    <v-card-title>
+    title
+    </v-card-title>
+    <v-card-subtitle>
+      subtutle
+    </v-card-subtitle>
+    <v-card-text>
+      descriptions of ad
+    </v-card-text>
+    <v-card-actions>
+      <v-btn>
+        add to shoplist
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "productItem",
+  props:{
+    product:Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
