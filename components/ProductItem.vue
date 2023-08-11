807a070f71240efa608c542d8e72f6bdcee81922
mohamaddnadr <amoo.nadr81@gmail.com>
2023-08-11 20:20:09 +0330

    create component for product

--- components/ProductItem.vue
+++ components/ProductItem.vue
@@ -1,30 +1,58 @@
 <template>
-  <div>
-  <v-card>
-    <v-card-title>
-    title
-    </v-card-title>
-    <v-card-subtitle>
-      subtutle
-    </v-card-subtitle>
-    <v-card-text>
-      descriptions of ad
-    </v-card-text>
-    <v-card-actions>
-      <v-btn>
-        add to shoplist
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-  </div>
+
+    <v-card>
+      <v-card-title>
+        {{ product.attributes.name }}
+<!--        name-->
+      </v-card-title>
+      <v-card-subtitle>
+        {{ product.attributes.display_price }}
+<!--        price-->
+      </v-card-subtitle>
+      <v-card-text>
+        {{product.attributes.description}}
+<!--        description-->
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          color="primary"
+        @click="addToCart(product)"
+        >
+add to Cart
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+
 </template>
 
 <script>
+import {mapActions} from 'vuex';
 export default {
   name: "productItem",
-  props:{
-    product:Object
+  props: {
+    product: Object
+  },
+  methods:{
+    async addToCart(item){
+      this.showS(
+        'added',
+        {
+          title: 'success',
+          variant: 'primary',
+          solid: true,
+          autoHideDelay: 3000,
+          noCloseButton: true,
+          toaster: 'b-toaster-top-center'
+        },
+      )
+      this.addToBasket(item)
+    },
+    ...mapActions(['addToBasket'])
+  },
+  computed:{
+
   }
+
 }
 </script>
 
